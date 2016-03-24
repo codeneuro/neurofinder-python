ef32e8ebc08e01b7c1a772956214357059ba241e
freeman <the.freeman.lab@gmail.com>
2016-03-23 22:37:16 -0400

    add threshold as an argument

--- neurofinder/commands/evaluate.py
+++ neurofinder/commands/evaluate.py
@@ -3,15 +3,20 @@ import json
 import click
 from .. import load, centers, shapes
 
-@click.argument('file1', nargs=1, metavar='<file1>', required=True)
-@click.argument('file2', nargs=1, metavar='<file2>', required=True)
+@click.argument('files', nargs=2, metavar='<files>', required=True)
+@click.option('--threshold', default=5, help='threshold distance')
 @click.command('evaluate', short_help='compare results of two algorithms', options_metavar='<options>')
-def evaluate(file1, file2):
-    a = load(file1)
-    b = load(file2)
-    precision, recall = centers(a, b)
-    overlap, exactness = shapes(a, b)
-    combined = 2 * (recall * precision) / (recall + precision)
+def evaluate(files, threshold):
+    a = load(files[0])
+    b = load(files[1])
+
+    recall, precision = centers(a, b, threshold=threshold)
+    inclusion, exclusion = shapes(a, b, threshold=threshold)
+
+    if recall == 0 and precision == 0:
+      combined = 0
+    else:
+      combined = 2 * (recall * precision) / (recall + precision)
     
-    result = {'combined': round(combined, 4), 'overlap': round(overlap, 4), 'precision': round(precision, 4), 'recall': round(recall, 4), 'exactness': round(exactness, 4)}
+    result = {'combined': round(combined, 4), 'inclusion': round(inclusion, 4), 'precision': round(precision, 4), 'recall': round(recall, 4), 'exclusion': round(exclusion, 4)}
     print json.dumps(result)
